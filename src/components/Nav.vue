8acc2ec2fdb7b9f800bf5bdf0e3ff77e590022fc
Robert Soriano <sorianorobertc@gmail.com>
2021-09-26 18:33:04 +0800

    fix toggle theme button

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -17,7 +17,7 @@
                         </path>
                     </svg>
                 </a>
-                <a class="mx-2 relative text-gray-700 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-300" href="#" @click.prevent="toggleDark">
+                <a class="mx-2 relative text-gray-700 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-300" href="#" @click.prevent="toggleDark()">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path v-if="isDark" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
                         <path v-else stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
